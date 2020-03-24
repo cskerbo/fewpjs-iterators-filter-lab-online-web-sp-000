bc710477f7a9a0fa7beb0b89563b71ec4908e4fe
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 22:06:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function findMatching(driverArray, string) {
-  const result = driverArray.filter(driver => driver == string);
+  const result = driverArray.filter(driver => driver.toUpperCase() == string.toUpperCase());
   return result
 }
