401f38ec3f0ac4443c410410664ac282ea93edfb
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 22:10:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function findMatching(driverArray, string) {
   const result = driverArray.filter(driver => driver.toUpperCase() == string.toUpperCase());
   return result
 }
+
+function fuzzyMatch(driverArray, string){
+  const result = driverArray.filter(driver => driver.charAt(0) === string)
+  return result
+}
