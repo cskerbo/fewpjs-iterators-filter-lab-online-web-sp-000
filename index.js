0f336bd3781582767ddabfeaba3779fb4e476a7c
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 22:13:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function findMatching(driverArray, string) {
 }
 
 function fuzzyMatch(driverArray, string){
-  const result = driverArray.filter(driver => driver.charAt(0) === string)
+  let stringLength = string.length;
+  const result = driverArray.filter(driver => driver.substring(0, stringLength) === string)
   return result
 }
